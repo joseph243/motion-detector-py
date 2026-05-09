a886fd643dcd26544103ecc96dd757dc10688e94
Joseph Vanderzwart <josephvanderzwart@protonmail.com>
2026-05-09 14:40:05 -0600

    bugfix!

--- motiondetect.py
+++ motiondetect.py
@@ -294,7 +294,7 @@ def main():
 			if command == "snapshot":
 				cameraprimer()
 				ret, snapshotimage = camera.read()
-				snapshotimage = encodeImageWithText(image2, current_time.strftime("%Y-%m-%d %H:%M:%S"))
+				snapshotimage = encodeImageWithText(snapshotimage, current_time.strftime("%Y-%m-%d %H:%M:%S"))
 				encodeImgSuccess, encoded = cv2.imencode('.jpg', snapshotimage)
 				if not encodeImgSuccess:
 					log("FAILURE ENCODING IMAGE FOR NOTIFICATION!!")
